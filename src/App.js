1e0adedb68cde1bcb66aa37513f6ac6a4235e7bd
Tomy <tomyleereyes@gmail.com>
2019-01-26 15:50:15 -0800

    enhance drop order with state

--- src/App.js
+++ src/App.js
@@ -8,8 +8,41 @@ class App extends Component {
   state = {
     initialData
   }
+
   onDragEnd = result => {
-    console.log('hey')
+    const { initialData } = this.state
+    const { destination, draggableId, source } = result
+    if (!destination) {
+      return
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return
+    }
+
+    const box = initialData.boxes[source.droppableId]
+    const newAnswerIds = Array.from(box.answersIds)
+    newAnswerIds.splice(source.index, 1)
+    newAnswerIds.splice(destination.index, 0, draggableId)
+
+    const newBox = {
+      ...box,
+      answersIds: newAnswerIds
+    }
+
+    const newState = {
+      ...this.state,
+      initialData: {
+        ...this.state.initialData,
+        boxes: {
+          [newBox.id]: newBox
+        }
+      }
+    }
+
+    this.setState(newState)
   }
 
   render() {
